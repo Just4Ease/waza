109a0aaceb2da484568d45628b0fee76a4ae7444
⚡️Nefe⚡️ <Just4Ease@users.noreply.github.com>
2023-09-29 23:28:46 +0100

    feat: updated the transactions model
    - introduced new fields to the transactions data model
    - more coming...

--- models/models.go
+++ models/models.go
@@ -13,19 +13,75 @@ type User struct {
 }
 
 type Account struct {
-	Id                 string
-	AccountName        string
-	AccountOwnerId     string
-	Currency           string
-	Iso2               string
-	Balance            float64
-	BalanceAfterDebit  float64
-	BalanceAfterCredit float64
+	Id                  string
+	AccountName         string
+	AccountOwnerId      string
+	Currency            string
+	Iso2                string
+	Balance             float64
+	BalanceBeforeCredit float64 // polled from transaction.
+	BalanceBeforeDebit  float64 // polled from transaction.
+	BalanceAfterDebit   float64 // polled from transaction.
+	BalanceAfterCredit  float64 // polled from transaction.
 }
 
 type Transaction struct {
+	Id                     string
+	Reference              string
+	Description            string
+	Amount                 float64
+	Type                   TransactionType
+	Status                 TransactionStatus
 	SourceAccountId        string
 	SourceAccountName      string
 	DestinationAccountId   string
 	DestinationAccountName string
+	BalanceBeforeCredit    float64
+	BalanceBeforeDebit     float64
+	BalanceAfterDebit      float64
+	BalanceAfterCredit     float64
+	TimeCreated            time.Time
+	TimeUpdated            time.Time
+}
+
+type TransactionType string
+
+const (
+	Credit TransactionType = "CREDIT"
+	Debit  TransactionType = "DEBIT"
+)
+
+func (t TransactionType) IsValid() bool {
+	switch t {
+	case Credit, Debit:
+		return true
+	default:
+		return false
+	}
+}
+
+func (t TransactionType) String() string {
+	return string(t)
+}
+
+type TransactionStatus string
+
+const (
+	Pending   TransactionStatus = "pending"
+	Completed TransactionStatus = "completed"
+	Canceled  TransactionStatus = "canceled"
+	Reversed  TransactionStatus = "reversed"
+)
+
+func (t TransactionStatus) IsValid() bool {
+	switch t {
+	case Pending, Completed, Canceled, Reversed:
+		return true
+	default:
+		return false
+	}
+}
+
+func (t TransactionStatus) String() string {
+	return string(t)
 }
